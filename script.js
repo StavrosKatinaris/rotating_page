61298362bcff37e2773970fbe8bc88ec7529a7d4
Stavros Katinaris <124068079+StavrosKatinaris@users.noreply.github.com>
2023-12-17 19:49:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const rotatingObject = document.getElementById('rotatingObject');
 const rightSection = document.getElementById('right-section');
 
 rightSection.addEventListener('scroll', () => {
-    const scrollPercentage = (rightSection.scrollTop / (rightSection.scrollHeight - rightSection.clientHeight)) * 100;
+    const scrollPercentage = (rightSection.scrollTop / (rightSection.scrollHeight - rightSection.clientHeight)) * 3000;
     const rotation = `rotate(${scrollPercentage}deg)`;
     rotatingObject.style.transform = rotation;
 });
